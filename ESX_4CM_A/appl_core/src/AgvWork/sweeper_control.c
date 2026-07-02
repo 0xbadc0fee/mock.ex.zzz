dc63e360b475427d8c44ef233c6694a23c2e2156
Silas Curfman <sgc98944@protonmail.com>
2026-07-02 08:29:07 -0700

    WIP sweeper control implementation tasks

--- ESX_4CM_A/appl_core/src/AgvWork/sweeper_control.c
+++ ESX_4CM_A/appl_core/src/AgvWork/sweeper_control.c
@@ -53,12 +53,17 @@ sint16 init_sweeperControl(T_CANDevices *_can_dev, T_Config_Sweeper *_nvmSweeper
 {
     sint16 s16_error = C_NO_ERR;
 
-    //populate local copy of NVM params
+    // FR-2.3 populate local copy of NVM params for scaling signal
     mt_sweeper.pt_nvmSweeper = _nvmSweeper;
 
     //populate local copy of RX HMI elements
     mt_sweeper.pu8_onOffCommand = &_can_dev->t_joystickJSLX.u8_b1_state;
+
+    // FR-2.1 Read requested speed from joystick slider via CAN
     mt_sweeper.ps16_requestedSpeed = &_can_dev->t_joystickJSLX.s16xPos;
+
+    // FR-2.3 Read PTO Engagement status from PGN 64932 (PTODE) SPN 3942
+    // FR-2.3 Read PTO Enable status from PGN 64932 (PTODE) SPN 3944
 
     return s16_error;
 
@@ -82,6 +87,14 @@ sint16 update_sweeperControl(void)
     // TODO-SGC get inputs joystick
     // TODO-SGC get inputs chassis / operator presence
     // TODO-SGC run sweeper at desired speed until shut off
+
+    // FR-2.3 Clamp and scale logic per config params
+
+    // FR-2.4 Override req speed to zero when PTO off
+
+    // FR-2.5 Apply time based ramp to smooth speed changes
+
+    // FR-2.6 Output to HW as PWM signal appropriate to sweeper coil specs
 
     return s16_error;
 }
